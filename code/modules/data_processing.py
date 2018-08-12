fixed the fq calculation, general things too

code/modules/data_processing.py
@@ -207,127 +207,125 @@ def divide_train_data(galaxies, data_keys, input_features, output_features, reds
         training_data_dict['train_weights'] = train_weights
         training_data_dict['val_weights'] = val_weights
         training_data_dict['test_weights'] = test_weights
-        
-    else:
-                
-        # store the relevant SSFR data
-        ssfr_directory = '/home/magnus/data/mock_data/ssfr/'
-        ssfr_data = {}
-        for redshift in training_data_dict['unique_redshifts']:
-            
-            file_name = 'galaxies.Z{:02.0f}'.format(redshift*10)
-            with open(ssfr_directory + file_name + '.json', 'r') as f:
-                ssfr = json.load(f)
-            
-            parameter_dict = ssfr.pop(0)
-            bin_widths = parameter_dict['bin_widths']
-            bin_edges = parameter_dict['bin_edges']
-            
-            bin_centers = [item[0] for item in ssfr]
-            mean_ssfr = [item[1] for item in ssfr]
-            errors = [item[2] for item in ssfr]
-            
-            redshift_data = {
-                'bin_centers': np.array(bin_centers),
-                'bin_widths': np.array(bin_widths),
-                'bin_edges': np.array(bin_edges),
-                'ssfr': np.array(mean_ssfr),
-                'errors': np.array(errors)
-            }
-                
-            ssfr_data['{:.1f}'.format(redshift)] = redshift_data
-            
-        training_data_dict['ssfr_data'] = ssfr_data
-        
-        # store the relevant SMF data
-        smf_directory = '/home/magnus/data/mock_data/stellar_mass_functions/'
-        smf_data = {}
-        
-        for redshift in training_data_dict['unique_redshifts']:
-            
-            file_name = 'galaxies.Z{:02.0f}'.format(redshift*10)
-            with open(smf_directory + file_name + '.json', 'r') as f:
-                smf_list = json.load(f)
-                
-            parameter_dict = smf_list.pop(0)
-            bin_widths = parameter_dict['bin_widths']
-            bin_edges = parameter_dict['bin_edges']
-            
-            bin_centers = [item[0] for item in smf_list]
-            smf = [item[1] for item in smf_list]
-            errors = [item[2] for item in smf_list]
-            
-            redshift_data = {
-                'bin_centers': np.array(bin_centers),
-                'bin_widths': np.array(bin_widths),
-                'bin_edges': np.array(bin_edges),
-                'smf': np.array(smf),
-                'errors': np.array(errors)
-            }
-            
-            smf_data['{:.1f}'.format(redshift)] = redshift_data
-            
-        training_data_dict['smf_data'] = smf_data
-        
-        # store the relevant FQ data
-        fq_directory = '/home/magnus/data/mock_data/fq/'
-        fq_data = {}
-        
-        for redshift in training_data_dict['unique_redshifts']:
-            
-            file_name = 'galaxies.Z{:02.0f}'.format(redshift*10)
-            with open(fq_directory + file_name + '.json', 'r') as f:
-                fq_list = json.load(f)
-                
-            parameter_dict = fq_list.pop(0)
-            bin_widths = parameter_dict['bin_widths']
-            bin_edges = parameter_dict['bin_edges']
-            
-            bin_centers = [item[0] for item in fq_list]
-            fq = [item[1] for item in fq_list]
-            errors = [item[2] for item in fq_list]
-            
-            redshift_data = {
-                'bin_centers': np.array(bin_centers),
-                'bin_widths': np.array(bin_widths),
-                'bin_edges': np.array(bin_edges),
-                'fq': np.array(fq),
-                'errors': np.array(errors)
-            }
-            
-            fq_data['{:.1f}'.format(redshift)] = redshift_data
-            
-        training_data_dict['fq_data'] = fq_data
-        
-        # store the relevant SHM data
-        shm_directory = '/home/magnus/data/mock_data/stellar_halo_mass_relations/'
-        shm_data = {}
-        
-        for redshift in training_data_dict['unique_redshifts']:
-            
-            file_name = 'galaxies.Z{:02.0f}'.format(redshift*10)
-            with open(shm_directory + file_name + '.json', 'r') as f:
-                shm_list = json.load(f)
-                
-            parameter_dict = shm_list.pop(0)
-            bin_widths = parameter_dict['bin_widths']
-            bin_edges = parameter_dict['bin_edges']
-            
-            bin_centers = [item[0] for item in shm_list]
-            shm = [item[1] for item in shm_list]
-            errors = [item[2] for item in shm_list]
-            
-            redshift_data = {
-                'bin_centers': np.array(bin_centers),
-                'bin_widths': np.array(bin_widths),
-                'bin_edges': np.array(bin_edges),
-                'shm': np.array(shm),
-                'errors': np.array(errors)
-            }
-            
-            shm_data['{:.1f}'.format(redshift)] = redshift_data
-            
-        training_data_dict['shm_data'] = shm_data
+                        
+    # store the relevant SSFR data
+    ssfr_directory = '/home/magnus/data/mock_data/ssfr/'
+    ssfr_data = {}
+    for redshift in training_data_dict['unique_redshifts']:
+
+        file_name = 'galaxies.Z{:02.0f}'.format(redshift*10)
+        with open(ssfr_directory + file_name + '.json', 'r') as f:
+            ssfr = json.load(f)
+
+        parameter_dict = ssfr.pop(0)
+        bin_widths = parameter_dict['bin_widths']
+        bin_edges = parameter_dict['bin_edges']
+
+        bin_centers = [item[0] for item in ssfr]
+        mean_ssfr = [item[1] for item in ssfr]
+        errors = [item[2] for item in ssfr]
+
+        redshift_data = {
+            'bin_centers': np.array(bin_centers),
+            'bin_widths': np.array(bin_widths),
+            'bin_edges': np.array(bin_edges),
+            'ssfr': np.array(mean_ssfr),
+            'errors': np.array(errors)
+        }
+
+        ssfr_data['{:.1f}'.format(redshift)] = redshift_data
+
+    training_data_dict['ssfr_data'] = ssfr_data
+
+    # store the relevant SMF data
+    smf_directory = '/home/magnus/data/mock_data/stellar_mass_functions/'
+    smf_data = {}
+
+    for redshift in training_data_dict['unique_redshifts']:
+
+        file_name = 'galaxies.Z{:02.0f}'.format(redshift*10)
+        with open(smf_directory + file_name + '.json', 'r') as f:
+            smf_list = json.load(f)
+
+        parameter_dict = smf_list.pop(0)
+        bin_widths = parameter_dict['bin_widths']
+        bin_edges = parameter_dict['bin_edges']
+
+        bin_centers = [item[0] for item in smf_list]
+        smf = [item[1] for item in smf_list]
+        errors = [item[2] for item in smf_list]
+
+        redshift_data = {
+            'bin_centers': np.array(bin_centers),
+            'bin_widths': np.array(bin_widths),
+            'bin_edges': np.array(bin_edges),
+            'smf': np.array(smf),
+            'errors': np.array(errors)
+        }
+
+        smf_data['{:.1f}'.format(redshift)] = redshift_data
+
+    training_data_dict['smf_data'] = smf_data
+
+    # store the relevant FQ data
+    fq_directory = '/home/magnus/data/mock_data/fq/'
+    fq_data = {}
+
+    for redshift in training_data_dict['unique_redshifts']:
+
+        file_name = 'galaxies.Z{:02.0f}'.format(redshift*10)
+        with open(fq_directory + file_name + '.json', 'r') as f:
+            fq_list = json.load(f)
+
+        parameter_dict = fq_list.pop(0)
+        bin_widths = parameter_dict['bin_widths']
+        bin_edges = parameter_dict['bin_edges']
+
+        bin_centers = [item[0] for item in fq_list]
+        fq = [item[1] for item in fq_list]
+        errors = [item[2] for item in fq_list]
+
+        redshift_data = {
+            'bin_centers': np.array(bin_centers),
+            'bin_widths': np.array(bin_widths),
+            'bin_edges': np.array(bin_edges),
+            'fq': np.array(fq),
+            'errors': np.array(errors)
+        }
+
+        fq_data['{:.1f}'.format(redshift)] = redshift_data
+
+    training_data_dict['fq_data'] = fq_data
+
+    # store the relevant SHM data
+    shm_directory = '/home/magnus/data/mock_data/stellar_halo_mass_relations/'
+    shm_data = {}
+
+    for redshift in training_data_dict['unique_redshifts']:
+
+        file_name = 'galaxies.Z{:02.0f}'.format(redshift*10)
+        with open(shm_directory + file_name + '.json', 'r') as f:
+            shm_list = json.load(f)
+
+        parameter_dict = shm_list.pop(0)
+        bin_widths = parameter_dict['bin_widths']
+        bin_edges = parameter_dict['bin_edges']
+
+        bin_centers = [item[0] for item in shm_list]
+        shm = [item[1] for item in shm_list]
+        errors = [item[2] for item in shm_list]
+
+        redshift_data = {
+            'bin_centers': np.array(bin_centers),
+            'bin_widths': np.array(bin_widths),
+            'bin_edges': np.array(bin_edges),
+            'shm': np.array(shm),
+            'errors': np.array(errors)
+        }
+
+        shm_data['{:.1f}'.format(redshift)] = redshift_data
+
+    training_data_dict['shm_data'] = shm_data
     
     return training_data_dict
 
@@ -515,7 +513,8 @@ def predict_points(model, training_data_dict, original_units=True, as_lists=Fals
         
 #     if len(training_data_dict['output_features']) == 1:
         
-    if original_units:
+    if original_units and 'conv_values_output' in training_data_dict:
+        print('hej')
         predicted_points = convert_units(predicted_norm_points, training_data_dict['norm']['output'], 
                                          back_to_original=True, conv_values=training_data_dict['conv_values_output'])
     else:
@@ -828,7 +827,7 @@ def binned_loss(training_data_dict, binning_feat, bin_feat, bin_feat_name, data_
             scale_factor = 1 / (1 + redshift)
 
             h_0 = 67.81 / (3.09e19) # 1/s
-            h_0 = h_0 * 60 * 24 * 365 # 1/yr
+            h_0 = h_0 * 60 * 60 * 24 * 365 # 1/yr
             h_r = h_0 * np.sqrt(1e-3*scale_factor**(-4) + 0.308*scale_factor**(-3) + 0*scale_factor**(-2) + 0.692)
             ssfr_cutoff = 0.3*h_r
             log_ssfr_cutoff = np.log10(ssfr_cutoff)
@@ -842,10 +841,6 @@ def binned_loss(training_data_dict, binning_feat, bin_feat, bin_feat_name, data_
                     fq = float('nan')
 
                 pred_bin_feat_dist[bin_num-1] = fq
-
-            ### TODO: check if the following line makes sense. Do we ever have zeros? Not just NaNs?
-            zero_indeces = [True if (frac == 0) else False for frac in pred_bin_feat_dist]
-            pred_bin_feat_dist[zero_indeces] = 1e-5
 
             non_nan_indeces = np.invert(np.isnan(pred_bin_feat_dist))
             
@@ -962,7 +957,7 @@ def binned_dist_func(training_data_dict, binning_feat, bin_feat, bin_feat_name, 
             scale_factor = 1 / (1 + redshift)
 
             h_0 = 67.81 / (3.09e19) # 1/s
-            h_0 = h_0 * 60 * 24 * 365 # 1/yr
+            h_0 = h_0 * 60 * 60 * 24 * 365 # 1/yr
             h_r = h_0 * np.sqrt(1e-3*scale_factor**(-4) + 0.308*scale_factor**(-3) + 0*scale_factor**(-2) + 0.692)
             ssfr_cutoff = 0.3*h_r
             log_ssfr_cutoff = np.log10(ssfr_cutoff)
@@ -977,15 +972,9 @@ def binned_dist_func(training_data_dict, binning_feat, bin_feat, bin_feat_name, 
 
                 pred_bin_feat_dist_redshift[bin_num-1] = fq
 
-            ### TODO: check if the following line makes sense. Do we ever have zeros? Not just NaNs?
-            zero_indeces = [True if (frac == 0) else False for frac in pred_bin_feat_dist_redshift]
-            pred_bin_feat_dist_redshift[zero_indeces] = 1e-5
-
-            non_nan_indeces = np.invert(np.isnan(pred_bin_feat_dist_redshift))
             
         else:    
             pred_bin_feat_dist_redshift = bin_means
-            non_nan_indeces = np.invert(np.isnan(pred_bin_feat_dist_redshift))
 
         pred_bin_feat_dist.append(pred_bin_feat_dist_redshift.copy())
         true_bin_feat_dist.append(true_bin_feat_dist_redshift.copy())
@@ -1044,35 +1033,39 @@ def loss_func_obs_stats(model, training_data_dict, loss_dict, real_obs=True, dat
 
         ############### mean SSFR ###############
 
-        loss_ssfr, dist_outside = \
-            binned_loss(training_data_dict, predicted_stellar_mass_log, ssfr_log, 'ssfr', data_type, loss_dict)
-        loss += loss_dict['ssfr_weight'] * loss_ssfr
-        dist_outside_tot += dist_outside
+        if loss_dict['ssfr_weight'] > 0:
+            loss_ssfr, dist_outside = \
+                binned_loss(training_data_dict, predicted_stellar_mass_log, ssfr_log, 'ssfr', data_type, loss_dict)
+            loss += loss_dict['ssfr_weight'] * loss_ssfr
+            dist_outside_tot += dist_outside
 #         nr_empty_bins =+ nr_empty_bins_ssfr
 
         ############### SMF ###############  
         
-        loss_smf, dist_outside = \
-            binned_loss(training_data_dict, predicted_stellar_mass_log, predicted_stellar_mass_log, 'smf', data_type, loss_dict)
-        loss += loss_dict['smf_weight'] * loss_smf
-        dist_outside_tot += dist_outside
+        if loss_dict['smf_weight'] > 0:
+            loss_smf, dist_outside = \
+                binned_loss(training_data_dict, predicted_stellar_mass_log, predicted_stellar_mass_log, 'smf', data_type, loss_dict)
+            loss += loss_dict['smf_weight'] * loss_smf
+            dist_outside_tot += dist_outside
 #         nr_empty_bins =+ nr_empty_bins_smf
 
         ############### FQ ###############
 
-        loss_fq, dist_outside = \
-            binned_loss(training_data_dict, predicted_stellar_mass_log, ssfr_log, 'fq', data_type, loss_dict)
-        loss += loss_dict['fq_weight'] * loss_fq
-        dist_outside_tot += dist_outside
+        if loss_dict['fq_weight'] > 0:
+            loss_fq, dist_outside = \
+                binned_loss(training_data_dict, predicted_stellar_mass_log, ssfr_log, 'fq', data_type, loss_dict)
+            loss += loss_dict['fq_weight'] * loss_fq
+            dist_outside_tot += dist_outside
 #         nr_empty_bins =+ nr_empty_bins_fq
 
         ############### SHM ###############
 
-        loss_shm, dist_outside = \
-            binned_loss(training_data_dict, training_data_dict['original_halo_masses_{}'.format(data_type)], 
-                                                               predicted_stellar_mass_log, 'shm', data_type, loss_dict)
-        loss += loss_dict['shm_weight'] * loss_shm
-        dist_outside_tot += dist_outside
+        if loss_dict['shm_weight'] > 0:
+            loss_shm, dist_outside = \
+                binned_loss(training_data_dict, training_data_dict['original_halo_masses_{}'.format(data_type)], 
+                                                                   predicted_stellar_mass_log, 'shm', data_type, loss_dict)
+            loss += loss_dict['shm_weight'] * loss_shm
+            dist_outside_tot += dist_outside
 #         nr_empty_bins =+ nr_empty_bins_fq
 
         loss /= loss_dict['ssfr_weight'] + loss_dict['smf_weight'] + loss_dict['fq_weight'] + loss_dict['shm_weight']
@@ -1088,7 +1081,7 @@ def plots_obs_stats(model, training_data_dict, real_obs=True, data_type='train',
     
     else:
         
-        y_pred = predict_points(model, training_data_dict, original_units=False, as_lists=False, data_type=data_type)
+        y_pred = predict_points(model, training_data_dict, original_units=True, as_lists=False, data_type=data_type)
         
         sfr_index = training_data_dict['output_features'].index('SFR')
         stellar_mass_index = training_data_dict['output_features'].index('Stellar_mass')

code/modules/pso_parallel_training_queue.py
@@ -1,7 +1,6 @@
 import os
 from os.path import expanduser
 home_dir = expanduser("~")
-network_path = home_dir + '/code/pso_code/trained_networks/'
 import numpy as np
 import time
 import datetime
@@ -35,10 +34,10 @@ class Feed_Forward_Neural_Network():
                                    pso_param_dict, self.reg_strength, reinf_learning, real_observations, nr_processes)
         
     def train_pso(self, nr_iterations, training_data_dict, speed_check=False, std_penalty=False, verbatim=False, 
-                  save_all_networks=False, draw_figures=True, loss_dict=None):
+                  draw_figures=True, loss_dict=None):
         
         self.pso_swarm.train_network(nr_iterations, training_data_dict,
-                                     std_penalty, speed_check, verbatim, save_all_networks, draw_figures, loss_dict)
+                                     std_penalty, speed_check, verbatim, draw_figures, loss_dict)
         
 
 class PSO_Swarm(Feed_Forward_Neural_Network):
@@ -74,6 +73,11 @@ class PSO_Swarm(Feed_Forward_Neural_Network):
         self.parent = parent
         self.reinf_learning = reinf_learning
         self.train_on_real_obs = real_observations
+        if real_observations:
+            self.obs_type = 'real_observations'
+        else:
+            self.obs_type = 'mock_observations'
+        self.model_path = '{}/trained_networks/pso_trained/{}/{}/'.format(home_dir, self.obs_type, self.parent.name)
         self.nr_processes = nr_processes
         self.nr_hidden_layers = nr_hidden_layers
         self.nr_neurons_per_lay = nr_neurons_per_lay
@@ -92,11 +96,10 @@ class PSO_Swarm(Feed_Forward_Neural_Network):
         
         self.vMax = (self.pso_param_dict['xMax'] - self.pso_param_dict['xMin']) / self.pso_param_dict['delta_t']
                 
-    def train_network(self, nr_iterations, training_data_dict, std_penalty, speed_check, verbatim, save_all_networks, draw_figs,
+    def train_network(self, nr_iterations, training_data_dict, std_penalty, speed_check, verbatim, draw_figs,
                       loss_dict):
         
         self.training_data_dict = training_data_dict
-        self.save_all_networks = save_all_networks
         
         self.nr_iterations_trained = nr_iterations
         self.std_penalty = std_penalty
@@ -104,7 +107,8 @@ class PSO_Swarm(Feed_Forward_Neural_Network):
         
         self.draw_figs = draw_figs
         
-        with open('progress.txt', 'w+') as f:
+        os.makedirs(os.path.dirname(self.model_path + 'progress.txt'), exist_ok=True)
+        with open(self.model_path + 'progress.txt', 'w+') as f:
             
             self.inp_queue = mp.Queue()
             self.results_queue = mp.Queue()
@@ -119,11 +123,11 @@ class PSO_Swarm(Feed_Forward_Neural_Network):
                                                                       self.network_args, self.weight_shapes, loss_dict))
                 process_list.append(process)
                 
-            distance_process = mp.Process(target=save_distances, args=(self.print_dist_queue, self.parent.name))
+            distance_process = mp.Process(target=save_distances, args=(self.print_dist_queue, self.model_path))
             process_list.append(distance_process)
             
             if draw_figs:
-                figure_drawer_process = mp.Process(target=figure_drawer, args=(self.figure_drawer_queue, self.parent.name, 
+                figure_drawer_process = mp.Process(target=figure_drawer, args=(self.figure_drawer_queue, self.model_path, 
                                                                                self.weight_shapes, self.network_args, 
                                                                                training_data_dict))
                 process_list.append(figure_drawer_process)
@@ -143,8 +147,8 @@ class PSO_Swarm(Feed_Forward_Neural_Network):
                 
                 self.progress = 0
                 
-                self.swarm_best_score_train = 1e20
-                self.swarm_best_score_val = 1e20
+                self.swarm_best_score_train = float('Inf')
+                self.swarm_best_score_val = float('Inf')
                 
                 self.swarm_best_distance_moved_p_point_one = []
                 self.swarm_best_distance_moved_p_one = []
@@ -238,17 +242,14 @@ class PSO_Swarm(Feed_Forward_Neural_Network):
                     'swarm_best_distance_moved_p_two': self.swarm_best_distance_moved_p_two,
                     'swarm_best_distance_moved_p_inf': self.swarm_best_distance_moved_p_inf
                 }
-                os.makedirs(os.path.dirname(network_path + self.parent.name + '/swarm_best_distance_moved.p'), exist_ok=True)
-                pickle.dump(distance_dict, open(network_path + self.parent.name + '/swarm_best_distance_moved.p', 'wb'))
+                os.makedirs(os.path.dirname(self.model_path + 'swarm_best_distance_moved.p'), exist_ok=True)
+                pickle.dump(distance_dict, open(self.model_path + 'swarm_best_distance_moved.p', 'wb'))
                 
                 self.swarm_best_pos_train = self.positions[i_particle]
                 self.time_since_train_improvement = 0
                 
-                if self.save_all_networks:
-                    self.inp_queue.put([self.positions[i_particle], i_particle, 
-                                        'save_all {} {:d} training'.format(self.parent.name, iteration)])
-                else:
-                    self.inp_queue.put([self.positions[i_particle], i_particle, 'save {}'.format(self.parent.name)])
+                self.inp_queue.put([self.positions[i_particle], i_particle, 
+                                    'save {:d} training {}'.format(iteration, self.model_path)])
                 out = self.results_queue.get(timeout=20)
 
                 if out != 'save_successful':
@@ -274,11 +275,8 @@ class PSO_Swarm(Feed_Forward_Neural_Network):
                         self.figure_drawer_queue.put([self.swarm_best_pos_val, iteration])
                     
                     # save the model
-                    if self.save_all_networks:
-                        self.inp_queue.put([self.positions[i_particle], i_particle, 
-                                            'save_all {} {:d} validation'.format(self.parent.name, iteration)])
-                    else:
-                        self.inp_queue.put([self.positions[i_particle], i_particle, 'save {}'.format(self.parent.name)])
+                    self.inp_queue.put([self.positions[i_particle], i_particle, 
+                                        'save {:d} validation {}'.format(iteration, self.model_path)])
                     out = self.results_queue.get(timeout=20)
 
                     if out != 'save_successful':
@@ -442,50 +440,25 @@ def particle_evaluator(inp_queue, results_queue, training_data_dict, reinf_learn
             weight_mat_list = get_weights(position, weight_shapes)
             model.set_weights(weight_mat_list)
 
-            str_list = string.split(' ')
+            str_list = string.split(' ')            
             
-            data_type = str_list[0]
             if len(str_list) == 1:
-                pass
-            elif len(str_list) == 2:
-                model_name = str_list[1]
-            elif len(str_list) == 3:
-                model_name = str_list[1]
-                iteration = str_list[2]
-            elif len(str_list) == 4:
-                model_name = str_list[1]
-                iteration = str_list[2]
-                mode_of_hs = str_list[3]
-            else:
-                print(string)
-                print(str_list)
-                print('wrong string sent')
-                break
-                
-            if data_type == 'train':
-                score = evaluate_model(model, training_data_dict, reinf_learning, train_on_real_obs, data_type=data_type, 
-                                       loss_dict=loss_dict)
-                results_queue.put([score, particle_nr])
-            elif data_type == 'val':
-                
+                data_type = str_list[0]
                 score = evaluate_model(model, training_data_dict, reinf_learning, train_on_real_obs, data_type=data_type,
                                        loss_dict=loss_dict)
-                y_pred = predict_points(model, training_data_dict, original_units=False, data_type=data_type)
-
-                stds = np.std(y_pred, axis=0)
+                if data_type == 'train':
+                    results_queue.put([score, particle_nr])
+                elif data_type == 'val':
+                    y_pred = predict_points(model, training_data_dict, original_units=False, data_type=data_type)
+                    stds = np.std(y_pred, axis=0)
+                    results_queue.put([score, stds])
                 
-                results_queue.put([score, stds])
+            elif len(str_list) == 4:
+                iteration = str_list[1]
+                mode_of_hs = str_list[2]
+                model_path = str_list[3]
                 
-            elif data_type == 'save':
-                
-                model.save(network_path + '{}.h5'.format(model_name))
-                pickle.dump(training_data_dict, open(network_path + '{}_training_data_dict.p'.format(model_name), 'wb'))
-                
-                results_queue.put('save_successful')
-                
-            elif data_type == 'save_all':
-                
-                directory = network_path + '{}/{}_best/'.format(model_name, mode_of_hs)
+                directory = '{}{}_best/'.format(model_path, mode_of_hs)
                 
                 if iteration == '0':
                     os.makedirs(os.path.dirname(directory + 'iter_{}.h5'.format(iteration)), exist_ok=True)
@@ -500,6 +473,12 @@ def particle_evaluator(inp_queue, results_queue, training_data_dict, reinf_learn
                 model.save(directory + 'iter_{}.h5'.format(iteration))
                 
                 results_queue.put('save_successful')
+                
+            else:
+                print(string)
+                print(str_list)
+                print('wrong string sent')
+                break
                 
 
 def get_weights(position, weight_shapes): # get the weight list corresponding to the position in parameter space
@@ -536,7 +515,7 @@ def evaluate_model(model, training_data_dict, reinf_learning, train_on_real_obs,
 
     return score
 
-def save_distances(queue, model_name):
+def save_distances(queue, model_path):
         
     keep_evaluating = True
     
@@ -553,13 +532,13 @@ def save_distances(queue, model_name):
             'p_two': p_two
         }
 
-        file_path = network_path + '{}/interparticle_distances/iter_{:d}.p'.format(model_name, iteration)
+        file_path = '{}interparticle_distances/iter_{:d}.p'.format(model_path, iteration)
 
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
         pickle.dump(distance_dict, open(file_path, 'wb'))
 
-def figure_drawer(queue, model_name, weight_shapes, network_args, training_data_dict):
+def figure_drawer(queue, model_path, weight_shapes, network_args, training_data_dict):
     
     input_features, output_features, nr_neurons_per_lay, nr_hidden_layers, \
                     activation_function, output_activation, reg_strength = network_args
@@ -575,7 +554,7 @@ def figure_drawer(queue, model_name, weight_shapes, network_args, training_data_
         model.set_weights(weight_mat_list)
         
         title = 'Iteration {}, best validation weights, validation data points shown'.format(iteration)
-        fig_file_path = network_path + '{}/figures_validation_weights/val_data/all_losses/iter_{}.png'.format(model_name, iteration)
+        fig_file_path = '{}figures_validation_weights/val_data/all_losses/iter_{}.png'.format(model_path, iteration)
         get_smf_ssfr_fq_plot(model, training_data_dict, title=title, data_type='val', full_range=True, save=True, 
                              file_path=fig_file_path, running_from_script=True)
         

code/scripts/reinforcement_training.py
@@ -15,10 +15,10 @@ np.random.seed(999)
 random.seed(999)
 
 ### General parameters
-total_set_size = 2.9e5 # how many examples will be used for training+validation+testing
-train_size = 1.5e5
-val_size = 1e5
-test_size = .4e5
+total_set_size = 2.9e4 # how many examples will be used for training+validation+testing
+train_size = 1.5e4
+val_size = 1e4
+test_size = .4e4
 input_features = ['Halo_mass', 'Halo_mass_peak', 'Scale_peak_mass', 'Scale_half_mass', 'Halo_growth_rate']#, 'Redshift']
 output_features = ['Stellar_mass', 'SFR']
 redshifts = [0]#,.1,.2,.5,1,2,3,4,6,8]
@@ -29,10 +29,8 @@ real_observations = False
 
 verbatim = True
 
-# network_name = 'testing'
 use_config_name = True
 # network_name = '{}'.format(datetime.datetime.now().strftime("%Y-%m-%d"))
-save_all_nets = True
 draw_figs = True
 
 ### Network parameters
@@ -42,18 +40,18 @@ output_activation = {'SFR': None, 'Stellar_mass': None}
 nr_neurons_per_layer = 6
 regularisation_strength = 1e-2
 std_penalty = False
-norm = {'input': 'zero_mean_unit_std',
-        'output': 'zero_mean_unit_std'} # 'none',   'zero_mean_unit_std',   'zero_to_one'
+norm = {'input': 'zero_mean_unit_std', # 'none',   'zero_mean_unit_std',   'zero_to_one'
+        'output': 'none'} # output norm does not matter for pso, only bp
 
 ### Loss parameters
 loss_dict = {
     'fq_weight': 1,
     'ssfr_weight': 1,
     'smf_weight': 1,
-    'shm_weight': 2,
+    'shm_weight': 1,
     'dist_outside_punish': 'exp',
-    'dist_outside_factor': 20,
-    'min_filled_bin_frac': 0.8
+    'dist_outside_factor': 10,
+    'min_filled_bin_frac': 0
 }
 
 ### PSO parameters
@@ -99,8 +97,8 @@ network = Feed_Forward_Neural_Network(nr_hidden_layers, nr_neurons_per_layer, in
                                       activation_function, output_activation, regularisation_strength, network_name)
 network.setup_pso(pso_param_dict, reinf_learning=reinforcement_learning, real_observations=real_observations, 
                   nr_processes=nr_processes)
-network.train_pso(nr_iterations, training_data_dict, std_penalty=std_penalty, verbatim=verbatim, save_all_networks=save_all_nets,
-                  draw_figures=draw_figs, loss_dict=loss_dict)
+network.train_pso(nr_iterations, training_data_dict, std_penalty=std_penalty, verbatim=verbatim, draw_figures=draw_figs, 
+                  loss_dict=loss_dict)
 
 
 